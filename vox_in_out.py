2e47769f0f459dcc7cf172c219f92a8633e4f4a9
KevinCaires <kevincaires@ymail.com>
2020-04-20 18:29:57 -0300

    FIX: Bug fixes.

--- vox_in_out.py
+++ vox_in_out.py
@@ -4,17 +4,18 @@ from os import path, system
 import pygame
 from speech_recognition import Recognizer, UnknownValueError, Microphone
 
-COMMAND_ONE = 'ADD'
+COMMAND_LIST = ['ADICIONAR', 'PARAR']
 
 def record_audio(audio):
     """
-    Função que responde o comando do ususário.
+    Função que grava a o aúdio de responsta para o usuário.
     """
     # Cria a pasta raiz dos audios caso a mesma não exista
     if not path.exists('audios'):
         system('mkdir audios')
 
-    # Atribui a entrada de aúdio e sua linguagem.
+    # Atribui a entrada de aúdio e sua linguagem. 
+    # O gTTS é um módulo da google para traduzir a escrita para a fala do robo.
     tts = gTTS(audio, lang='pt-br')
     # Salva a entrada no formato mp3.
     tts.save('audios/audio_response.mp3')
@@ -25,7 +26,8 @@ def get_audio():
     """
     # Função que reconhece a entrada da fala.
     mic_in = Recognizer()
-    # Pega a entrada do microfone
+    # Pega a entrada do microfone.
+    # Ele vai ouvir o comando e traduzir em escrita para que as demais funções possam iterar com o comando.
     with Microphone() as source:
         # Ajusta a entrada eliminando ruidos.
         mic_in.adjust_for_ambient_noise(source)
@@ -34,10 +36,16 @@ def get_audio():
         system('clear')
         
         # Mostra na tela os comandos padrões.
-        print('Comandos:')
-        print(COMMAND_ONE)
-        for info in get_command_list():
-            print(info)
+        print('Comandos do sistema:')
+        for system_commands in COMMAND_LIST:
+            print(system_commands)
+
+        user_commands = [info for info in get_command_list()]
+        
+        if user_commands:
+            print('Comandos do usuário:')
+            for comand in user_commands:
+                print(comand)
 
         # Armazena a informação de audio na variável.
         audio = mic_in.listen(source)
@@ -48,10 +56,13 @@ def get_audio():
             print(f'Entrada de aúdio: {phrase}')
             
             # Condição para o caso de o usuário use o comando ADD.
-            if str(phrase).upper() == COMMAND_ONE:
+            if str(phrase).upper() == COMMAND_LIST[0]:
                 set_command()
 
                 return get_audio()
+            
+            if str(phrase).upper() == COMMAND_LIST[1]:
+                return system('exit')
             
             if str(phrase).upper() not in get_command_list():
                 record_audio('Esse comando não está cadastrado')
